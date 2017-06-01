1df59006be05cc2d026748929b5be63973c3fe64
Guillermo <mrog0@outlook.com>
2017-06-01 06:45:40 +0100

    Add feature test for viewing links in /links

---
+++ spec/listing_list_spec.rb
@@ -0,0 +1,11 @@
+feature 'viewing the list of links' do
+  scenario 'I can see the list of links' do
+    Link.create(url: 'www.google.com', name: 'Google')
+    visit('/links')
+
+    # sanity check
+    expect(page.status_code).to eq 200
+
+    expect(page).to have_content('www.google.com')
+  end
+end
